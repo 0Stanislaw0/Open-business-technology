a05f57c6a9d98776c71c9148e5fe1c2f46b178e4
Stanislaw <52160413+0Stanislaw0@users.noreply.github.com>
2020-03-30 19:15:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="m3",
+    name="m3", #
     version="0.1.1",
     author="Stanislav",
     author_email="stas.yyyy20@gmail.com",
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    install_requires=['django==1.11','m3-objectpack==2.2.25'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
